73eac7b7a2f28eff82367d517afc8f1a97f88d42
Nebiyu <nebiyu28@gmail.com>
2023-06-08 11:11:38 +0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 extern crate piston_window;
 extern crate rand;
 mod draw;
+mod snake;
+mod game;
+mod food;
+use piston_window::*;
+use piston_window::types::Color;
+use game::Game;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (30, 30);
+    let mut window: PistonWindow = WindowSettings::new(
+        "Snake",
+        [draw::to_coord_u32(width), draw::to_coord_u32(height)],
+    )   .exit_on_esc(true)
+        .build()
+        .unwrap(); 
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    } 
 }
